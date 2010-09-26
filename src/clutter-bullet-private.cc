dd4bdc9d9a74cbacf6ea406e691bf3b31942bc6b
William Hua <huaw@mcmaster.ca>
2010-09-26 19:13:27 -0400

    need to double check if btMotionState works

--- src/clutter-bullet-private.cc
+++ src/clutter-bullet-private.cc
@@ -28,6 +28,11 @@
 
 
 
+#define DEG(x) (180 / M_PI * (x))
+#define RAD(x) (M_PI / 180 * (x))
+
+
+
 ClutterBulletMotionState::ClutterBulletMotionState (ClutterActor *a)
 : actor (a)
 {
@@ -40,22 +45,25 @@ ClutterBulletMotionState::getWorldTransform (btTransform &t) const
 {
   CoglMatrix    a;
   ClutterVertex x;
-  btVector3     y;
+  gfloat        w;
+  gdouble       rx, ry, rz;
 
   clutter_actor_get_size (actor, &x.x, &x.y);
+  clutter_actor_get_transformation_matrix (actor, &a);
 
   x.x /= 2;
   x.y /= 2;
   x.z  = 0;
+  w    = 1;
 
-  clutter_actor_get_transformation_matrix (actor, &a);
+  cogl_matrix_transform_point (&a, &x.x, &x.y, &x.z, &w);
 
-  y.setX (a.xx * x.x + a.xy * x.y + a.xz * x.z + a.xw);
-  y.setY (a.yx * x.x + a.yy * x.y + a.yz * x.z + a.yw);
-  y.setZ (a.zx * x.x + a.zy * x.y + a.zz * x.z + a.zw);
-  y.setW (0);
+  g_object_get (actor, "rotation-angle-x", &rx,
+                       "rotation-angle-y", &ry,
+                       "rotation-angle-z", &rz, NULL);
 
-  t.setOrigin (y);
+  t.setOrigin (btVector3 (x.x / w, x.y / w, x.z / w));
+  t.getBasis ().setEulerZYX (RAD (rx), RAD (ry), RAD (rz));
 }
 
 
@@ -63,4 +71,36 @@ ClutterBulletMotionState::getWorldTransform (btTransform &t) const
 void
 ClutterBulletMotionState::setWorldTransform (const btTransform &t)
 {
+  CoglMatrix    a;
+  ClutterVertex x, dx;
+  btScalar      rx0, ry0, rz0;
+  gdouble       rx1, ry1, rz1;
+  gfloat        w, z;
+
+  t.getBasis ().getEulerZYX (rz0, ry0, rx0);
+
+  rx1 = DEG (rx0);
+  ry1 = DEG (ry0);
+  rz1 = DEG (rz0);
+
+  g_object_set (actor, "rotation-angle-x", rx1,
+                       "rotation-angle-y", ry1,
+                       "rotation-angle-z", rz1, NULL);
+
+  clutter_actor_get_size (actor, &x.x, &x.y);
+  clutter_actor_get_transformation_matrix (actor, &a);
+
+  x.x /= 2;
+  x.y /= 2;
+  x.z  = 0;
+  w    = 1;
+
+  cogl_matrix_transform_point (&a, &x.x, &x.y, &x.z, &w);
+
+  dx.x = t.getOrigin ().x () - x.x / w;
+  dx.y = t.getOrigin ().y () - x.y / w;
+  dx.z = t.getOrigin ().z () - x.z / w;
+
+  clutter_actor_move_by (actor, dx.x, dx.y);
+  clutter_actor_set_depth (actor, clutter_actor_get_depth (actor) + dx.z);
 }
