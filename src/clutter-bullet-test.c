83bacfa245e8945ba21197dbae57a4ac470a98ea
William Hua <huaw@mcmaster.ca>
2010-09-30 22:56:54 -0400

    a better physics test

--- src/clutter-bullet-test.c
+++ src/clutter-bullet-test.c
@@ -39,12 +39,15 @@ main (int   argc,
     { 0, 0, 0, 255 },
     { 255, 0, 0, 255 },
     { 0, 255, 0, 255 },
-    { 0, 0, 255, 255 }
+    { 0, 0, 255, 255 },
+    { 255, 255, 0, 255 }
   };
 
   ClutterActor *actor = NULL;
 
   clutter_init (&argc, &argv);
+
+  cogl_set_depth_test_enabled (TRUE);
 
   stage = clutter_stage_get_default ();
   clutter_stage_set_color (CLUTTER_STAGE (stage), colour);
@@ -54,20 +57,34 @@ main (int   argc,
   clutter_container_add_actor (CLUTTER_CONTAINER (stage), group);
 
   actor = clutter_rectangle_new_with_color (colour + 1);
-  clutter_actor_set_position (actor, 150, 0);
-  clutter_actor_set_depth (actor, 0);
-  clutter_actor_set_size (actor, 200, 100);
+  clutter_actor_set_position (actor, 100, 0);
+  clutter_actor_set_depth (actor, 10);
+  clutter_actor_set_size (actor, 100, 100);
+  clutter_actor_set_rotation (actor, CLUTTER_X_AXIS, 20, 50, 50, 0);
   actor = clutter_bullet_card_new (actor, 1);
   clutter_container_add_actor (CLUTTER_CONTAINER (group), actor);
 
-  /*
   actor = clutter_rectangle_new_with_color (colour + 2);
+  clutter_actor_set_position (actor, 150, 0);
+  clutter_actor_set_depth (actor, 0);
+  clutter_actor_set_size (actor, 200, 100);
+  clutter_actor_set_rotation (actor, CLUTTER_X_AXIS, 10, 100, 50, 0);
+  actor = clutter_bullet_card_new (actor, 1);
+  clutter_container_add_actor (CLUTTER_CONTAINER (group), actor);
+
+  actor = clutter_rectangle_new_with_color (colour + 3);
   clutter_actor_set_position (actor, 0, 250);
   clutter_actor_set_depth (actor, 0);
   clutter_actor_set_size (actor, 200, 100);
-  clutter_actor_set_rotation (actor, CLUTTER_X_AXIS, 80, 100, 50, 0);
+  clutter_actor_set_rotation (actor, CLUTTER_X_AXIS, 90, 100, 50, 0);
   clutter_container_add_actor (CLUTTER_CONTAINER (group), actor);
-  */
+
+  actor = clutter_rectangle_new_with_color (colour + 4);
+  clutter_actor_set_position (actor, 200, 350);
+  clutter_actor_set_depth (actor, 0);
+  clutter_actor_set_size (actor, 200, 100);
+  clutter_actor_set_rotation (actor, CLUTTER_X_AXIS, 90, 100, 50, 0);
+  clutter_container_add_actor (CLUTTER_CONTAINER (group), actor);
 
   clutter_bullet_group_start (CLUTTER_BULLET_GROUP (group));
 
