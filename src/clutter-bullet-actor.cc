79db9b1935c918f63e0229f7953e42238635620a
William Hua <huaw@mcmaster.ca>
2010-10-08 15:21:35 -0400

    resolve the Very Bad Thing

--- src/clutter-bullet-actor.cc
+++ src/clutter-bullet-actor.cc
@@ -46,6 +46,8 @@ struct _ClutterBulletActorBinder
 
 
 
+static GHashTable *actor_binder;
+
 static GHashTable *actor_body;
 
 
@@ -130,15 +132,31 @@ void
 clutter_bullet_actor_bind (ClutterActor       *self,
                            ClutterBulletGroup *group)
 {
-  ClutterBulletActorBinder *binder = new ClutterBulletActorBinder;
+  ClutterBulletActorBinder *binder;
 
-  binder->actor = self;
-  binder->group = group;
+  if (actor_binder == NULL)
+    actor_binder = g_hash_table_new (NULL, NULL);
 
-  binder->signal = g_signal_connect (clutter_bullet_actor_get_actor (self),
-                                     "notify::allocation",
-                                     G_CALLBACK (clutter_bullet_actor_real_bind),
-                                     binder);
+  binder = (ClutterBulletActorBinder *) g_hash_table_lookup (actor_binder, self);
+
+  if (binder == NULL)
+  {
+    binder = new ClutterBulletActorBinder;
+
+    binder->actor = self;
+    binder->group = group;
+
+    binder->signal = g_signal_connect (
+      clutter_bullet_actor_get_actor (self),
+      "notify::allocation",
+      G_CALLBACK (clutter_bullet_actor_real_bind),
+      binder
+    );
+
+    g_hash_table_replace (actor_binder, self, binder);
+  }
+  else
+    binder->group = group;
 }
 
 
@@ -155,7 +173,12 @@ clutter_bullet_actor_real_bind (GObject    *obj,
   ClutterActor             *self   = binder->actor;
   ClutterBulletGroup       *group  = binder->group;
 
+  if (actor_binder == NULL)
+    actor_binder = g_hash_table_new (NULL, NULL);
+
   g_signal_handler_disconnect (obj, binder->signal);
+  g_hash_table_remove (actor_binder, self);
+  delete binder;
 
   if (CLUTTER_BULLET_IS_ACTOR (self))
   {
@@ -201,8 +224,6 @@ clutter_bullet_actor_real_bind (GObject    *obj,
       g_hash_table_replace (actor_body, self, body);
     }
   }
-
-  delete binder;
 }
 
 
@@ -211,6 +232,18 @@ void
 clutter_bullet_actor_unbind (ClutterActor       *self,
                              ClutterBulletGroup *group)
 {
+  ClutterBulletActorBinder *binder;
+
+  if (actor_binder == NULL)
+    actor_binder = g_hash_table_new (NULL, NULL);
+
+  if ((binder = (ClutterBulletActorBinder *) g_hash_table_lookup (actor_binder, self)) != NULL)
+  {
+    g_signal_handler_disconnect (clutter_bullet_actor_get_actor (self), binder->signal);
+    g_hash_table_remove (actor_binder, self);
+    delete binder;
+  }
+
   if (CLUTTER_BULLET_IS_ACTOR (self))
   {
     ClutterBulletActor *actor = CLUTTER_BULLET_ACTOR (self);
