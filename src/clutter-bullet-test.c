0adbeffbaa123b779be2418f3c0df399dfa6086a
William Hua <huaw@mcmaster.ca>
2010-11-12 10:31:08 -0500

    test the new hinge joint

--- src/clutter-bullet-test.c
+++ src/clutter-bullet-test.c
@@ -40,11 +40,12 @@ main (int   argc,
     { 255, 0, 0, 255 },
     { 0, 255, 0, 255 },
     { 0, 0, 255, 255 },
-    { 255, 255, 0, 255 }
+    { 255, 255, 0, 255 },
+    { 255, 0, 255, 255 }
   };
 
-  ClutterActor  *actor[2];
-  ClutterVertex  pivot[2];
+  ClutterActor  *actor[4];
+  ClutterVertex  pivot[4];
 
   clutter_init (&argc, &argv);
 
@@ -87,21 +88,29 @@ main (int   argc,
   clutter_actor_set_rotation (actor[0], CLUTTER_X_AXIS, 90, 100, 50, 0);
   clutter_container_add_actor (CLUTTER_CONTAINER (group), actor[0]);
 
-  actor[0] = clutter_rectangle_new_with_color (colour + 1);
-  clutter_actor_set_position (actor[0], 300, 0);
-  clutter_actor_set_depth (actor[0], -10);
-  clutter_actor_set_size (actor[0], 200, 100);
-  clutter_actor_set_rotation (actor[0], CLUTTER_X_AXIS, 0, 100, 50, 0);
-  /* actor[0] = clutter_bullet_card_new (actor[0]); */
-  clutter_container_add_actor (CLUTTER_CONTAINER (group), actor[0]);
-
-  actor[1] = clutter_rectangle_new_with_color (colour + 2);
-  clutter_actor_set_position (actor[1], 500, 50);
-  clutter_actor_set_depth (actor[1], 40);
-  clutter_actor_set_size (actor[1], 100, 100);
-  clutter_actor_set_rotation (actor[1], CLUTTER_X_AXIS, 90, 50, 50, 0);
-  actor[1] = clutter_bullet_card_new (actor[1]);
+  actor[1] = clutter_rectangle_new_with_color (colour + 1);
+  clutter_actor_set_position (actor[1], 300, 0);
+  clutter_actor_set_depth (actor[1], -10);
+  clutter_actor_set_size (actor[1], 200, 100);
+  clutter_actor_set_rotation (actor[1], CLUTTER_X_AXIS, 0, 100, 50, 0);
   clutter_container_add_actor (CLUTTER_CONTAINER (group), actor[1]);
+
+  actor[2] = clutter_rectangle_new_with_color (colour + 2);
+  clutter_actor_set_position (actor[2], 506, 56);
+  clutter_actor_set_depth (actor[2], 40);
+  clutter_actor_set_size (actor[2], 100, 100);
+  clutter_actor_set_rotation (actor[2], CLUTTER_X_AXIS, 90, 50, 50, 0);
+  actor[2] = clutter_bullet_card_new (actor[2]);
+  clutter_container_add_actor (CLUTTER_CONTAINER (group), actor[2]);
+
+  actor[3] = clutter_rectangle_new_with_color (colour + 5);
+  clutter_actor_set_position (actor[3], 356, 300);
+  clutter_actor_set_depth (actor[3], 0);
+  clutter_actor_set_size (actor[3], 100, 100);
+  clutter_actor_set_rotation (actor[3], CLUTTER_X_AXIS, -1, 50, 50, 0);
+  clutter_actor_set_rotation (actor[3], CLUTTER_Y_AXIS, 90, 50, 50, 0);
+  actor[3] = clutter_bullet_card_new (actor[3]);
+  clutter_container_add_actor (CLUTTER_CONTAINER (group), actor[3]);
 
   pivot[0].x = 103;
   pivot[0].y = 53;
@@ -110,7 +119,24 @@ main (int   argc,
   pivot[1].y = 53;
   pivot[1].z = 0;
 
-  clutter_bullet_pivot_new (actor[0], pivot + 0, actor[1], pivot + 1);
+  clutter_bullet_pivot_new (actor[1], pivot + 0,
+                            actor[2], pivot + 1);
+
+  pivot[0].x = 103;
+  pivot[0].y = 53;
+  pivot[0].z = 0;
+  pivot[1].x = 104;
+  pivot[1].y = -53;
+  pivot[1].z = 0;
+  pivot[2].x = -53;
+  pivot[2].y = 53;
+  pivot[2].z = 0;
+  pivot[3].x = 53;
+  pivot[3].y = 53;
+  pivot[3].z = 0;
+
+  clutter_bullet_hinge_new (actor[0], pivot + 0, pivot + 1,
+                            actor[3], pivot + 2, pivot + 3);
 
   clutter_bullet_group_start (CLUTTER_BULLET_GROUP (group));
 
